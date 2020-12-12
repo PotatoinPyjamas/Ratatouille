aec664c7c4d8ae00783e33ef16651c3e530abced
Shruti Nk <68746915+PotatoinPyjamas@users.noreply.github.com>
2020-12-12 21:46:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ lst3 = []
 def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
     return lst3
-for i in range(2):
+'''for i in range(2):
     temp = input()
-    lst3.append(temp)
+    lst3.append(temp)'''
 
 @app.route('/', methods=['GET', 'POST'])
 def values():
     d = {}
-    d = intersection(lst1, lst3)
+    d = intersection(lst1, lst2)
     return jsonify(d)
 
 
